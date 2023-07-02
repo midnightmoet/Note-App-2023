8b1d236d81ec292ad9de5c633678acf892604158
Lisa Miner <midnightmoet@gmail.com>
2023-07-01 20:53:55 -0400

    Saving progress

---
+++ script.js
@@ -0,0 +1,7 @@
+const appEl = document.getElementById('app');
+const noteEl = document.getElementById('note');
+const textareaEl = document.getElementById('textarea');
+const saveBtnEl = document.getElementById('saveBtn');
+const deleteBtnEl = document.getElementById('deleteBtn');
+const addBtnEl = document.getElementById('addBtn');
+const editBtnEl = document.getElementById('editBtn');
